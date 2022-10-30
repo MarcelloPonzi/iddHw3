33b065c8901bc37a816d8f248c283b60ad67fc67
AleSajo <alessandro.sajeva@gmail.com>
2022-10-30 18:30:36 +0100

    Merger iniziato, manca ordinamento e estrazione delle prime k tabelle. Non testato.

---
+++ src/iddHw3/Merger.java
@@ -0,0 +1,55 @@
+package iddHw3;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.*;
+import org.apache.lucene.store.Directory;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.*;
+
+public class Merger {
+
+    private IndexReader reader;
+
+    private IndexSearcher searcher;
+
+    private Map<String, Integer> set2count;
+
+    public Merger(Directory directory) throws IOException {
+        this.reader = DirectoryReader.open(directory);
+        this.searcher = new IndexSearcher(reader);
+        this.set2count = new HashMap<String, Integer>();
+    }
+
+    public void merge(LinkedList<String> termList, int size) throws IOException {
+
+        for (String term : termList) {
+
+            System.out.println("Ricerca in corso sul termine: "+term);
+
+            Query query = new TermQuery(new Term("keywords", term));
+            TopDocs hits = searcher.search(query,size);
+
+            for(ScoreDoc scoreDoc : hits.scoreDocs) {
+                Document doc = searcher.doc(scoreDoc.doc);
+                String id = String.valueOf(doc.get("id"));
+
+                if (this.set2count.containsKey(id)) {
+                    this.set2count.put(id, this.set2count.get(id) + 1);
+                    System.out.println("Incremento contatore per termine " + term);
+                }
+                else {
+                    set2count.put(id, 1);
+                }
+            }
+
+            // a questo punto set2count completato
+
+
+        }
+    }
+}
