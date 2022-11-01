0c7418bb42ff50052be6d93020d43db9091c1d3a
AleSajo <alessandro.sajeva@gmail.com>
2022-11-01 11:36:01 +0100

    Iniziata classe Statistiche

--- src/iddHw3/Statistiche.java
+++ src/iddHw3/Statistiche.java
@@ -2,6 +2,20 @@ package iddHw3;
 
 public class Statistiche {
 
+    //TODO numero di tabelle
 
+    //TODO numero medio di righe
+
+    //TODO numero medio di colonne
+
+    //TODO numero medio di valori nulli per tabella
+
+    //TODO Distribuzione numero di righe (quante tabelle hanno 1,2,3,4, etc. righe)
+
+    //TODO Distribuzione numero di colonne (quante tabelle hanno 1, 2, 3, 4, etc. colonne)
+
+    //TODO Distribuzione valori distinti (quante colonne hanno 1, 2, 3, 4, etc valori distinti)
+
+    //TODO Altro a vostra scelta
 
 }
